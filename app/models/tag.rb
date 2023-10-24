fdda2e4349cfb339504a0df3037d094b46a8b70d
B <54575305+marybrunette@users.noreply.github.com>
2023-10-24 11:11:59 +0800

    set tag color and course color

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,13 @@
 class Tag < ApplicationRecord
   belongs_to :course
+
+  def tag_color
+    "#{name}-#{color}"
+  end
+
+  def course_color
+    "#{title}-#{color}"
+  end
+
+  delegate :title, to: :course
 end
